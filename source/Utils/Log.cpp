0acb06ea64bc754a573b5f2e006904a141875bf3
tschuls <juliast@ethz.ch>
2015-05-11 10:48:15 +0200

    added correct time logging for VS compiler

--- source/Utils/Log.cpp
+++ source/Utils/Log.cpp
@@ -2,15 +2,36 @@
 #include <iostream>
 #include <sstream>
 
+#include <Windows.h>
+
+
+int gettimeofday(struct timeval * tp, struct timezone * tzp)
+{
+    // Note: some broken versions only have 8 trailing zero's, the correct epoch has 9 trailing zero's
+    static const uint64_t EPOCH = ((uint64_t) 116444736000000000ULL);
+
+    SYSTEMTIME  system_time;
+    FILETIME    file_time;
+    uint64_t    time;
+
+    GetSystemTime( &system_time );
+    SystemTimeToFileTime( &system_time, &file_time );
+    time =  ((uint64_t)file_time.dwLowDateTime )      ;
+    time += ((uint64_t)file_time.dwHighDateTime) << 32;
+
+    tp->tv_sec  = (long) ((time - EPOCH) / 10000000L);
+    tp->tv_usec = (long) (system_time.wMilliseconds * 1000);
+    return 0;
+}
 
 
 MyCPUTimer::MyCPUTimer(){
-    //gettimeofday(&m_tim, NULL);  
-    //m_starTime=m_tim.tv_sec;//+(m_tim.tv_usec/1000000.0);  
+    gettimeofday(&m_tim, NULL);  
+    m_starTime=m_tim.tv_sec;//+(m_tim.tv_usec/1000000.0);  
 }
 double MyCPUTimer::elapsed(){
-    //gettimeofday(&m_tim, NULL);  
-    //return (m_tim.tv_sec)-m_starTime;//+(m_tim.tv_usec/1000000.0));  
+    gettimeofday(&m_tim, NULL);  
+    return (m_tim.tv_sec)-m_starTime;//+(m_tim.tv_usec/1000000.0));  
   return 1;
 }
 
